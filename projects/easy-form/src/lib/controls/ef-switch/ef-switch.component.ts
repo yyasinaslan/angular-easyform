e642b2512a481390467d5a1b19f92ff877f75069
BirselE <birsel.epik@gmail.com>
2024-03-08 10:03:30 +0300

    feat(easy-form): switch component created and styles added

---
+++ projects/easy-form/src/lib/controls/ef-switch/ef-switch.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-ef-switch',
+  standalone: true,
+  imports: [],
+  templateUrl: './ef-switch.component.html',
+  styleUrl: './ef-switch.component.css'
+})
+export class EfSwitchComponent {
+
+}
