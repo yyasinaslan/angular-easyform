e80e142a7bc7f3b41f2917a7cf8e3718f33c659d
BirselE <birsel.epik@gmail.com>
2024-03-21 11:33:25 +0300

    feat(easy-form): landing page

--- src/app/website/home/home.component.ts
+++ src/app/website/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterLink} from "@angular/router";
 
 @Component({
@@ -11,5 +11,24 @@ import {RouterLink} from "@angular/router";
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  codeToCopy = "npm i @yyasinaslan/easyform";
+  isCopied = false;
+
+  copyText() {
+    navigator.clipboard.writeText(this.codeToCopy)
+      .then(() => {
+        //console.log("Text copied successfully!", this.isCopied);
+        this.isCopied = true;
+      })
+      .catch(err => {
+        //console.error("Copy failed:", err, this.isCopied);
+      })
+      .finally(() => {
+        setTimeout(() => {
+          this.isCopied = false;
+        }, 2000);
+      });
+  }
 
 }
